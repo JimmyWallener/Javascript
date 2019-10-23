e10bc2d3a39bc4a3f181e6cb91e7fb013af92c52
Jimmy Wallener <jimmy.wallener@protonmail.com>
2019-10-23 13:29:36 +0200

    removed value from button
    removed value from button since its obselete

---
+++ calculator/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * 1. set variable to hold inputdata - convert input to parseFloat
+ * 2. set variable to hold memorydata - convert input to parseFloat
+ * 3. create a switch function getNumber() for numberical input
+ * 3.a create switch function for operational getOp()
+ * 4. do a conditional statement concerning use of decimal "." so that it cant be used several times
+ * 5. create if statement that allows use of decimal point before numerical input
+ * 6. create function that displays input & memorydata
+ * 7. create a clearAll() function that resets all dataput
+ * 8. create a backspace function backSpace() which uses a .slice for removing last input.
+ * 9.
+ *
+ *
+ * DOM or call function onclick?
+ * ? : Use class to create callbacks to functions?
+ */
