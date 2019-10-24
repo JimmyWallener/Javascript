a7228cbd4ba8239528cc063bee3d061e57b76f50
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2019-10-24 09:59:13 +0200

    updated script.js

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -14,3 +14,30 @@
  * DOM or call function onclick?
  * ? : Use class to create callbacks to functions?
  */
+
+const numberSelector = document.querySelectorAll("number");
+const operatorSelector = document.querySelectorAll("operator");
+const backspaceSelection = document.getElementById("delete");
+const outputToScreen = document.getElementById("outputScreen").innerHTML
+  .numberSelector;
+const inputToScreen = document.getElementById("inputScreen").innerHTML
+  .numberSelector;
+
+function getNumbers(num) {};
+
+
+
+function getOperator(oper) {};
+
+
+function calculate();
+
+
+
+function outPut();
+
+
+function inPut();
+
+
+function display();
