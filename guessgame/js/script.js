23a7979300ec773e5f8f8d13ce6fd805ee11ae95
Jimmy Wallener <jimmy.wallener@protonmail.com>
2019-11-12 18:27:27 +0100

    finished quiz

--- guessgame/js/script.js
+++ guessgame/js/script.js
@@ -1,3 +1,5 @@
+// Questions from https://www.dn.se/nyheter/fragesport/ar-du-smartare-an-en-10-aring/
+
 const quizData = [
   {
     title: "Ett rovdjur är ett djur som äter andra djur. Vilket av dessa är ett rovdjur?",
@@ -20,7 +22,7 @@ const quizData = [
     correct: 1
   },
   {
-    title: "Isbjörnar och valrossar ser väldigt olika ut, men båda kan överleva där det är mycket kallt.\n En isbjörn har en tjock päls som hjälper den att hålla sig varm. Valrossen har ingen tjock päls.\n Vad håller valrossen varm?",
+    title: "Vad håller valrossen varm?",
     answer: [
       "Fettlager",
       "Betar",
@@ -50,7 +52,7 @@ const quizData = [
     correct: 3
   },
   {
-    title: "Vetenskapsmän tror att haven en gång täckte mycket av det som nu är land.\n Vilken av dessa saker som hittades på land ledde till att vetenskapsmän tror det?",
+    title: "Vilken av dessa saker som hittades på land som fått vetenskapsmän att tro att land legat under havet en gång i tiden?",
     answer: [
       "Vattengrottor",
       "Sandig jord",
@@ -94,75 +96,96 @@ const quizData = [
     answer: [
       "28",
       "23",
-      "32"
+      "32",
+      "27"
     ],
     correct: 0
   }
 ]
-
+// Setting inital value 
 
 let totals = 0;
 let activeQuestion = 0;
-let randomQuestion = Math.floor(Math.random() * quizData.length); // Creates a randomiser based on the lenght of the array.
 
-$(document).ready(function () {
-  $('.start a').click(function (event) {
-    event.preventDefault(); // Stops the href from loading a page
-    $('.start').hide(); // Hides start button after click
-    $('.quiz').show(); // shows quiz div with content
-    displayQuestion(); // runs function for displaying questions
-  });
+// Javascript functions with Jquery
 
-  $('.quiz ul').on('click', 'li', function () { // On click on list, add .selected to ul for css padding
-    $('.selected').removeClass('selected'); // If selected is active, remove it
-    $(this).addClass('selected'); // just add class selected (css style)
-    $('.quiz p').html(''); // Once something is selected, remove nothing selected prompt
-  });
-  $('.quiz a').click(function (event) {
-    event.preventDefault();
-    if ($('li.selected').length) {
-      let guess = parseInt($('li.selected').attr("id")); // create a var that holds the id from the selected answer and convert it to Int.
-      isAnswerCorrect(guess);
-    } else {
-      $('.quiz p').text("You need to select an answer"); // If nothing has been selected, prompt user
-    }
-  })
-});
-
+// Function that loops through the questions and appends the question answers in a list.
 
 function displayQuestion() {
-  let question = quizData[activeQuestion]; // Randomiser on the questions
-  $('.quiz h2').text(question.title); // Adds title from array to h2
-  $('.quiz ul').html(''); // clears ul from dummylists
-  for (var i = 0; i < question.answer.length; i++) {
-    $('.quiz ul').append("<li id='" + i + "'>" + question.answer[i] + "</li>"); // Appends a list with questions and adds index to each
+  let question = quizData[activeQuestion];
+  let answers = question.answer;
+  $('.quiz h2').text(question.title);
+  $('.quiz ul').html(''); // clears the list before adding new <li> elements to <ul>
+  for (var i = 0; i < answers.length; i++) {
+    $('.quiz ul').append("<li id='" + i + "'>" + question.answer[i] + "</li>");
 
   }
 }
+// function that checks the answer and increments totals if correct, also increments activeQuestion for the quiz to move on to the next question.
+// adds an if-statement that checks if activeQuestion index is equal or greater then quiz lenght and if so, stops quiz and shows stats, else it moves on to the next question.
 
 function isAnswerCorrect(guess) {
   let question = quizData[activeQuestion];
-  let numberOfQuestions = quizData;
   if (question.correct === guess) {
     totals++;
   }
   activeQuestion++;
-  if (activeQuestion >= numberOfQuestions.length) {
+  if (activeQuestion >= quizData.length) {
     showTotals();
   } else {
     displayQuestion();
   }
-  console.log(activeQuestion);
-  console.log(question.correct)
-  console.log(numberOfQuestions.length);
-  console.log(question);
 }
-
+// Shows total score and switches from quiz div to summary div and displays different congratulatory statements depending on score
 function showTotals() {
   let question = quizData;
+  console.log(question);
   $('.quiz').hide();
   $('.summary').show();
-  $('.summary p').text("Congratulations! You have scored " + totals + " out of " + question.length + " correct!");
+  if (totals === question.length) {
+    $('.summary p').addClass('result').text(`Gratulerar! Du är ett geni som klarade ${totals} av ${question.length} frågor.`);
+  } else if (totals < 5) {
+    $('.summary p').addClass('result').text(`Ojdå, kanske dags plugga igen? Du klarade ${totals} av ${question.length} frågor.`);
+  } else {
+    $('.summary p').addClass('result').text(`Gratulerar! Du klarade ${totals} av ${question.length} frågor.`);
+  }
 }
+
+// Qjuery with eventlisteners
+
+$(document).ready(function () {
+  $('.start a').click(function (event) {
+    event.preventDefault(); // Stops the href from loading a page
+    $('.start').hide();
+    $('.quiz').show();
+    displayQuestion();
+  });
+
+  $('.quiz ul').on('click', 'li', function () { // Adding eventlistener that targets single elements (instead of .click).
+    $('.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $('.quiz p').html(''); // removes the prompt sett below if user actually selects an element
+  });
+
+  $('.quiz a').click(function (event) { // Adding eventlistener that checks for the answer
+    event.preventDefault();
+    if ($('li.selected').length) { // Checks if the clickable is selected or not with .lenght
+      let guess = parseInt($('li.selected').attr("id")); // convert string value to int for comparison
+      isAnswerCorrect(guess);
+    } else {
+      $('.quiz p').text("Du måste ange ett svars alternativ"); // If nothing has been selected, prompt user
+    }
+  });
+  $('.summary a').click(function (event) { // Adding eventlistener för restart of quiz
+    event.preventDefault();
+    $('.summary').hide();
+    $('.start').show();
+    totals = 0;
+    activeQuestion = 0;
+  });
+});
+
+
+
 
 
